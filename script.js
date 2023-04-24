191643259b1f961dbea618521c8c045b8115dc10
dannydelts94 <126992132+dannydelts94@users.noreply.github.com>
2023-04-23 20:30:24 -0400

    final html

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ var hum = document.querySelector('.hum')
 var win = document.querySelector('.win')
 var APIKey = '7b8a9bb54b18ada4466416f59d222bd4';
 
-button.addEventListener('click', function() {
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=7b8a9bb54b18ada4466416f59d222bd4')
-.then(response => response.json())
-.then (data => console.log(data))
+button.addEventListener('click', function () {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=7b8a9bb54b18ada4466416f59d222bd4')
+        .then(response => response.json())
+        .then(data => console.log(data))
 
 })
 
